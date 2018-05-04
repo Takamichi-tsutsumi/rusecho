95292bfa6a430ece8386190c6ac27856ea53ba35
Takamichi-tsutsumi <takamichi_tsutsumi@waku-2.com>
2018-05-04 18:45:32 +0900

    remove commented code

--- src/main.rs
+++ src/main.rs
@@ -24,24 +24,4 @@ fn main() {
                  });
 
     tokio::run(server);
-
-    // let addr = "127.0.0.1:8080".parse().unwrap();
-    // let listner: TcpListener = TcpListener::bind(&addr).expect("unable to bind TCP Listner");
-    // println!("Listening on port 8080");
-    //
-    // let server = listner
-    //     .incoming()
-    //     .map_err(|e| eprintln!("accept failed = {:?}", e))
-    //     .for_each(|sock| {
-    //         let (reader, writer) = sock.split();
-    //
-    //         let bytes_copied = copy(reader, writer);
-    //         let handle_conn = bytes_copied
-    //             .map(|amt| println!("wrote {:?}", amt))
-    //             .map_err(|err| eprintln!("IO error {:?}", err));
-    //
-    //         tokio::spawn(handle_conn)
-    //     });
-    //
-    // tokio::run(server);
 }
